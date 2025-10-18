f3661b68224c541c4c4029837ffb3d6ef64306bb
bonxom <bonxom@gmail.com>
2025-10-18 23:21:58 +0700

    UI update, distributed sys

--- client/src/component/Game.js
+++ client/src/component/Game.js
@@ -63,51 +63,85 @@ import Board from "./Board";
 //     return move;
 // }
 
-const calculateWinner = (board) => {
-    const winnerSet = [
-        [0, 1, 2],
-        [3, 4, 5],
-        [6, 7, 8],
-        [0, 3, 6],
-        [1, 4, 7],
-        [2, 5, 8],
-        [0, 4, 8],
-        [2, 4, 6]
-    ]
+// const calculateWinner = (board) => {
+//     const winnerSet = [
+//         [0, 1, 2],
+//         [3, 4, 5],
+//         [6, 7, 8],
+//         [0, 3, 6],
+//         [1, 4, 7],
+//         [2, 5, 8],
+//         [0, 4, 8],
+//         [2, 4, 6]
+//     ]
 
-    for (const [idx1, idx2, idx3] of winnerSet){
-        if (board[idx1] && board[idx1] === board[idx2] && board[idx2] === board[idx3]){
-            return board[idx1];
-        }
-    }
+//     for (const [idx1, idx2, idx3] of winnerSet){
+//         if (board[idx1] && board[idx1] === board[idx2] && board[idx2] === board[idx3]){
+//             return board[idx1];
+//         }
+//     }
 
-    if (!board.includes(null)){
-        return 'Draw';
-    }
-    return null;
-}
+//     if (!board.includes(null)){
+//         return 'Draw';
+//     }
+//     return null;
+// }
 
 function Game(){
     const [board, setBoard] = useState(Array(9).fill(null));
     // const [xIsNext, setXIsNext] = useState(true);
-    const [player, setPlayer] = userState(null);
+    const [player, setPlayer] = useState(null);
     const [winner, setWinner] = useState(null);
+    const [isPlayable, setIsPlayable] = useState(false);
     const wsRef = useRef(null);
+    const playerRef = useRef(null);
 
+    useEffect( () =>{
+        playerRef.current = player;
+    }, [player])
 
     useEffect( () => {
         if (wsRef.current){ //ws will run 2 time, one for test, and one for real run 
             return; 
         }   
         const ws = new WebSocket('ws://localhost:8081');
-        ws.current = ws;
+        wsRef.current = ws;
 
         ws.onopen = () => {
-            ws.send("Hello server");
+            // ws.send("Hello server");
             ws.send(JSON.stringify({
-                type: 'prepare' 
-            }))
+                type: 'prepare',
+            }));
+            // console.log('init1 ' + player);
+            // if (player === 'X') setIsPlayable(true); -> wrong
         };
+
+        ws.onmessage = (event) => {
+            const { type, data } = JSON.parse(event.data);
+
+            switch (type){
+                case 'start':
+                    console.log('data: ', data);
+                    setPlayer(data.player);
+                    setIsPlayable(data.player === 'X');
+                    playerRef.current = data.player;
+                    console.log('init2 ' + player); //can not set value for player -> player will be set in the next render
+                    console.log('init3' + data.player);
+                    break;
+                case 'display':
+                    console.log(data);
+                    setBoard(data.board);
+                    // console.log('can player play: ', player);
+                    // setIsPlayable(player === data.nextPlayer); -> wrong: player is not set yet
+                    setIsPlayable(playerRef.current === data.nextPlayer);
+                    if (data.winner) {
+                        setWinner(data.winner);
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
 
         return () => {// call when component unmount 
             ws.close();
@@ -133,20 +167,42 @@ function Game(){
     // }, [xIsNext])
 
     const handleClick = (index) => {
-        if (board[index] !== null || winner) return;
-        board[index] = "X";
-        setXIsNext(!xIsNext);
+        if (board[index] !== null || winner || !isPlayable) return;
+        // board[index] = "X";
+        // setXIsNext(!xIsNext);
+        board[index] = player;
         console.log(board);
         // setBoard(board); //same reference -> not set
         const tmp = [...board] //copy board
         setBoard(tmp);
+
+        wsRef.current.send(JSON.stringify({
+            data: {
+                board: tmp,
+                nextPlayer: player === 'X' ? 'O' : 'X',
+            },
+            type: 'move',
+        }));
     }
 
     return(
-        <div>
+        <div className="main">
             <h2>{winner ? winner : "N/A"}</h2>
-            <Board board={board} handleClick={handleClick}/>
-
+            <div className="game">
+                <span className="player">{
+                    (!player) ? 
+                        "You are the spectator":
+                        ("You are player" + {player})}</span>
+                <span className="turn">{
+                    (player != null) ?
+                        ((winner) ? 
+                            ( (player === winner)? "You win" : "You lose" )
+                            :(isPlayable ? 'Your turn' : 'Wait for opponent'))
+                        :"Keep watching!"
+                }
+                </span>
+                <Board board={board} handleClick={handleClick}/>
+            </div>
         </div>
     );
 }
--- server/index.js
+++ server/index.js
@@ -1,11 +1,74 @@
 
-const WebSocket = require("ws")
-const server = new WebSocket.Server({ port: 8081 })
-console.log("bonx")
+const WebSocket = require("ws");
+const server = new WebSocket.Server({ port: 8081 });
+console.log("bonx");
+
+const PLAYER = ['X', 'O'];
+const sockets = new Map(); // ws -> X || O
+
+const assignPlayer = (ws) => {
+    if (sockets.size == 2) return null; //full slot
+
+    if (sockets.size == 0) {
+        sockets.set(ws, 'X'); 
+        return 'X';
+    } 
+    sockets.set(ws, 'O');
+    return 'O';
+}
+
+const calculateWinner = (board) => {
+    const winnerSet = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ]
+
+    for (const [idx1, idx2, idx3] of winnerSet){
+        if (board[idx1] && board[idx1] === board[idx2] && board[idx2] === board[idx3]){
+            return board[idx1];
+        }
+    }
+
+    if (!board.includes(null)){
+        return 'Draw';
+    }
+    return null;
+}
 
 server.on('connection', (ws) => {
     console.log("connected");
     ws.on('message', (msg) => {
-        console.log(msg.toString());
+        const { type, data } = JSON.parse(msg);
+
+        switch (type) {
+            case 'prepare':
+                ws.send(JSON.stringify({ //send a object {...}
+                    type: 'start',
+                    data: {
+                        player: assignPlayer(ws),
+                    },
+                }));
+                break;
+            case 'move':
+                server.clients.forEach((socket) => {
+                    socket.send(JSON.stringify({
+                        type: 'display',
+                        data: {
+                            board: data.board,
+                            nextPlayer: data.nextPlayer,
+                            winner: calculateWinner(data.board),
+                        },
+                    }));
+                })
+                break;
+            default:
+                break;
+        }
     });
 })
